1a3122538c95f7379385e22d0267ff2ed2e3797f
Purnima Yadav <purnimayadavyaadav@gmail.com>
2025-12-06 22:16:47 +0530

    HomeHeroText part completed

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Video from "./Video";
 
 const HomeHeroText = () => {
   return (
-    <div>HomeHeroText</div>
-  )
-}
+    <div className="font-[font2] pt-5 text-center ">
+      <div className="text-[9.5vw] justify-center flex item-center uppercase leading-[8vw]">L'étincelle</div>
+      <div className="text-[9.5vw] justify-center flex items-start uppercase leading-[8vw]">qui <div className="h-[8vw] w-[16vw] rounded-full -mt-1 overflow-hidden"><Video/></div> génère</div>
+      <div className="text-[9.5vw] justify-center flex item-center uppercase leading-[8vw]">la créativité</div>
+    </div>
+  );
+};
 
-export default HomeHeroText
+export default HomeHeroText;
