384b27b6d39b60a7854442d734c9ff6474c89005
Purnima Yadav <purnimayadavyaadav@gmail.com>
2025-12-06 12:18:47 +0530

    working...

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Video = () => {
   return (
-    <div>Video</div>
+    <div className='h-full w-full'>
+        <video className='h-full w-full object-cover' autoPlay loop muted src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1765002666~exp=1765006266~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=9fc2926cef44a0d9062091eae91003f1c762aa3c4ba94422b6a70bbc1182a2bc&r=dXMtZWFzdDE%3D"></video>
+    </div>
   )
 }
 
