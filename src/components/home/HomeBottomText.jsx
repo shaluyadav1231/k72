3d3e4994a868eb2da9179a9b2d59250916612b00
Purnima Yadav <purnimayadavyaadav@gmail.com>
2025-12-07 10:22:57 +0530

    homeBottomText part completed

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomeBottomText = () => {
   return (
-    <div>HomeBottomText</div>
+    <div className='font-[font2] flex items-center justify-center gap-2'>
+      <div className='border-3  hover:border-[#D3FD50] hover:text-[#D3FD50] h-44 flex item-center px-10 border-white rounded-full uppercase'>
+      <Link className='text-[6vw] mt-6' to='/projects'>Projects</Link>
+      </div>
+      <div className='border-3  hover:border-[#D3FD50] hover:text-[#D3FD50] h-44 flex item-center px-10 border-white rounded-full uppercase'>
+      <Link className='text-[6vw] mt-6' to='/agence'>Agence</Link>
+      </div>
+    </div>
   )
 }
 
