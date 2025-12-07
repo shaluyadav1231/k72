908e2fd583005e3daff1f3e8af5701d79b03a75e
Purnima Yadav <purnimayadavyaadav@gmail.com>
2025-12-07 11:59:20 +0530

    working...

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const HomeBottomText = () => {
   return (
     <div className='font-[font2] flex items-center justify-center gap-2'>
-      <div className='border-3  hover:border-[#D3FD50] hover:text-[#D3FD50] h-44 flex item-center px-10 border-white rounded-full uppercase'>
+      <div className='border-3  hover:border-[#D3FD50] hover:text-[#D3FD50] h-44 flex item-center px-14 border-white rounded-full uppercase'>
       <Link className='text-[6vw] mt-6' to='/projects'>Projects</Link>
       </div>
-      <div className='border-3  hover:border-[#D3FD50] hover:text-[#D3FD50] h-44 flex item-center px-10 border-white rounded-full uppercase'>
+      <div className='border-3  hover:border-[#D3FD50] hover:text-[#D3FD50] h-44 flex item-center px-14 border-white rounded-full uppercase'>
       <Link className='text-[6vw] mt-6' to='/agence'>Agence</Link>
       </div>
     </div>
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -2,8 +2,41 @@ import React from 'react'
 
 const Agence = () => {
   return (
-    <div>Agence</div>
+<div>
+  <div className='section1'>
+  <div className='absolute overflow-hidden h-[20vw] rounded-3xl w-[15vw]  top-96 left-[30vw] bg-red-500'>
+    <img className='h-full object-cover w-full ' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7 " alt="" />
+  </div>
+    <div className=' relative font-[font-2]'>
+    <div className='mt-[55vh]'>
+      <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>Soixan7e <br />
+Douze</h1>
+    </div>
+    <div className='pl-[40%] mt-20'>
+      <p className='text-6xl'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.
+      </p>
+    </div>
+  </div>  
+</div>
+<div className="section2 h-screen">
+  <div className='section1'>
+  <div className='absolute overflow-hidden h-[20vw] rounded-3xl w-[15vw]  top-96 left-[30vw] bg-red-500'>
+    <img className='h-full object-cover w-full ' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7 " alt="" />
+  </div>
+    <div className='relative font-[font-2]'>
+    <div className='mt-[55vh]'>
+      <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>Soixan7e <br />
+Douze</h1>
+    </div>
+    <div className='pl-[40%] mt-20'>
+      <p className='text-6xl'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.
+      </p>
+    </div>
+  </div>  
+</div>
+</div>
+</div>
   )
-}
+}                                                       
 
 export default Agence
