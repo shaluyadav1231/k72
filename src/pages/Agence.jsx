17b0fb4538764c1f9e098bf7951fbc6497b60404
Purnima Yadav <purnimayadavyaadav@gmail.com>
2025-12-12 11:32:39 +0530

    working in agence page

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -10,6 +10,21 @@ const Agence = () => {
   const imageDiveRef = useRef(null)
   const imageRef = useRef(null)
 
+const imageArray=[
+    'https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64',
+    'https://k72.ca/images/teamMembers/Lawrence_480x640.jpg?w=480&h=640&fit=crop&s=0a878205586092164001a9afe0ef4007',
+    'https://k72.ca/images/teamMembers/HugoJoseph_480x640.jpg?w=480&h=640&fit=crop&s=f152025b8a59b062d1e7978b5d6544c3',
+    'https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991',
+    'https://k72.ca/images/teamMembers/SophieA_480x640.jpg?w=480&h=640&fit=crop&s=fcb556060c29623e706dfbc4eeca87ac',
+    'https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2',
+    'https://k72.ca/images/teamMembers/MEL_480X640.jpg?w=480&h=640&fit=crop&s=07c9bfee89816720b873e6748a276af6',
+    'https://k72.ca/images/teamMembers/CAMILLE_480X640_2.jpg?w=480&h=640&fit=crop&s=74317575b2d72fd11c5296615c383e4a',
+    'https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847',
+    'https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6',
+    
+]
+
+
   useGSAP(function(){
    
     gsap.to(imageDiveRef.current,{
@@ -41,9 +56,9 @@ Douze</h1>
 </div>
 <div className="section2 h-screen">
   <div className='section1'>
-  <div className='absolute overflow-hidden h-[20vw] rounded-3xl w-[15vw]  top-96 left-[30vw] bg-red-500'>
+  {/* <div className='absolute overflow-hidden h-[20vw] rounded-3xl w-[15vw]  top-96 left-[30vw] bg-red-500'>
     <img className='h-full object-cover w-full ' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7 " alt="" />
-  </div>
+  </div> */}
     <div className='relative font-[font-2]'>
     <div className='mt-[55vh]'>
       <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>Soixan7e <br />
