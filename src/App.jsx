4195181778b449a55dea74c5f28c095a1a83cd33
Purnima Yadav <purnimayadavyaadav@gmail.com>
2025-12-14 22:38:23 +0530

    start..

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,31 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const App = () => {
+
+useGSAP(function(){
+  const tl = gsap.timeline()
+  tl.from('.stair',{
+    height:0,
+    stagger:{
+      amount: -0.3
+    }
+  })
+})
+
   return (
     <div className='text-white'>
+      <div className='h-screen w-full flex fixed z-20 top-0'>
+        <div className=' stair h-full w-1/2 bg-black'></div>
+        <div className=' stair h-full w-1/2 bg-black'></div>
+        <div className=' stair h-full w-1/2 bg-black'></div>
+        <div className=' stair h-full w-1/2 bg-black'></div>
+        <div className=' stair h-full w-1/2 bg-black'></div>
+
+      </div>
       <Routes >
         <Route path='/' element={<Home />} />
         <Route path='/agence' element={<Agence />} />
