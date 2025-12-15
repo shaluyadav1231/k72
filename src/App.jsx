f7290fce03bab2f849248529c1d31d982add6233
Purnima Yadav <purnimayadavyaadav@gmail.com>
2025-12-15 10:15:44 +0530

    working with gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useReducer, useRef } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
@@ -8,8 +8,17 @@ import gsap from 'gsap'
 
 const App = () => {
 
+const stairParesntRef = useRef(null)
+
+
 useGSAP(function(){
   const tl = gsap.timeline()
+
+tl.to(stairParesntRef.current,{
+    display:'block'
+  })
+
+
   tl.from('.stair',{
     height:0,
     stagger:{
@@ -19,13 +28,19 @@ useGSAP(function(){
   tl.to('.stair',{
     y:'100%',
     stagger:{
-      amount:-0.3
+      amount:-0.25
     }
+  })
+  tl.to(stairParesntRef.current,{
+    display:'none'
+  })
+   tl.to('.stair',{
+    y:'0%',
   })
 })
 
   return (
-    <div className='text-white'>
+    <div ref={stairParesntRef} className='text-white'>
       <div className='h-screen w-full fixed z-20 top-0'>
         <div className='h-full w-full flex'>
         <div className=' stair h-full w-1/2 bg-black'></div>
