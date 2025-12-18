3e97533fb587d67bd154b9c07c57a3f518b7bd3a
Purnima Yadav <purnimayadavyaadav@gmail.com>
2025-12-18 20:59:12 +0530

    full screen nav

--- src/components/Navigation/FullScreenNav.jsx
+++ src/components/Navigation/FullScreenNav.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const FullScreenNav = () => {
   return (
-    <div>FullScreenNav</div>
-  )
-}
+    <div id="fullscreennav">
+      <div id="all-links">
+        <div className="link">
+          <h1>Projects</h1>
+          <div>
+            <div>
+            <h2>Pour Tout voir</h2>
+            <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b"alt=""/>
+            <h2>Pour Tout voir</h2>
+            <img src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=640&h=290&s=755b635c06d126151d64017fa1042a7cb"alt=""/>
+          </div>
+            <div>
+            <h2>Pour Tout voir</h2>
+            <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b"alt=""/>
+            <h2>Pour Tout voir</h2>
+            <img src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=640&h=290&s=755b635c06d126151d64017fa1042a7cb"alt=""/>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default FullScreenNav
+export default FullScreenNav;
