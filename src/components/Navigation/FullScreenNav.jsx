fb0e547443c59e112b02e1b6ddc84749c3dcdc39
Purnima Yadav <purnimayadavyaadav@gmail.com>
2025-12-20 10:01:03 +0530

    working...

--- src/components/Navigation/FullScreenNav.jsx
+++ src/components/Navigation/FullScreenNav.jsx
@@ -2,25 +2,20 @@ import React from "react";
 
 const FullScreenNav = () => {
   return (
-    <div id="fullscreennav">
-      <div id="all-links">
-        <div className="link">
-          <h1>Projects</h1>
-          <div>
-            <div>
-            <h2>Pour Tout voir</h2>
-            <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b"alt=""/>
-            <h2>Pour Tout voir</h2>
-            <img src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=640&h=290&s=755b635c06d126151d64017fa1042a7cb"alt=""/>
-          </div>
-            <div>
-            <h2>Pour Tout voir</h2>
-            <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b"alt=""/>
-            <h2>Pour Tout voir</h2>
-            <img src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=640&h=290&s=755b635c06d126151d64017fa1042a7cb"alt=""/>
+    <div id="fullscreennav" className="text-white h-screen  py-40 w-full absolute bg-amber-900">
+      <div className="">
+        <div className="relative border-t-1 border-white">
+          <h1 className="font-[font-2] text-[8vw] text-center leading-[0.8] pt-1 uppercase">Projects</h1>
+          <div className="absolute text-black flex top-0 bg-[#D3FD50]">
+            <div className="flex overflow-auto item-center">
+            <h2 className=" whitespace-nowrap font-[font-2] text-[8vw] text-center leading-[0.8] pt-1 uppercase">Pour Tout voir</h2>
+            <img className="h-36 rounded-full shrink-0  w-96 object-cover" src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b"alt=""/>
+            <h2 className=" whitespace-nowrap font-[font-2] text-[8vw] text-center leading-[0.8] pt-1 uppercase">Pour Tout voir</h2>
+            <img className="h-36 rounded-full shrink-0  w-96 object-cover" src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=640&h=290&s=755b635c06d126151d64017fa1042a7c"alt=""/>
           </div>
           </div>
         </div>
+       
       </div>
     </div>
   );
